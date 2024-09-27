db2f553fd6be52cf358d93ac37f7dc1117aa1631
Srinivasan Sundaram <srinivasan.sundaram1@digital.homeoffice.gov.uk>
2024-09-27 14:32:07 +0100

    Modified to add the aws connection and the services to be monitored.

--- main.tf
+++ main.tf
@@ -8,12 +8,20 @@ terraform {
 }
 
 resource "dynatrace_aws_credentials" "aws_connection" {
-  for_each       = var.aws_connections
-  label          = each.key
+  label          = var.tenant_vars.dynatrace_creds_label
   partition_type = "AWS_DEFAULT"
   tagged_only    = false
+  running_on_dynatrace_infrastructure = true
+  remove_defaults = true
   authentication_data {
-    account_id = each.value.account_id
-    iam_role   = each.value.role_name
+    account_id = var.tenant_vars.aws_acc_id
+    iam_role   = var.tenant_vars.aws_iam_role
   }
+}
+
+resource "dynatrace_aws_service" "monitoredservices" {
+  for_each = toset(var.tenant_vars.services_to_watch)
+  credentials_id = dynatrace_aws_credentials.aws_connection.id
+  use_recommended_metrics = true
+  name           = each.key
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,7 @@
-variable "aws_connections" {
-  description = "A map of AWS Connections to create. The key is the name of the connection."
-  type = map(object({
-    account_id = string
-    role_name  = string
-  }))
-  default = {}
+variable "tenant_vars" {
+  type = any
 }
+
+#variable "aws_region" {
+#  type = string
+#}
