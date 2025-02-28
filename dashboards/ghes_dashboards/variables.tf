7938ddf18b3ad4534d29b8ec24ae12569e620fdb
MOHAMMED-ho <mohammed.shareef@digital.homeoffice.gov.uk>
2025-02-28 13:53:41 +0000

    adding variable for group name

--- dashboards/ghes_dashboards/variables.tf
+++ dashboards/ghes_dashboards/variables.tf
@@ -2,3 +2,8 @@ variable "ghes_hostname" {
   type        = string
   description = "GHES dashboard hostname"
 }
+variable "dt_admin_group_name" {
+  type = string
+   description = "DT IAM Group Name"
+  
+}
