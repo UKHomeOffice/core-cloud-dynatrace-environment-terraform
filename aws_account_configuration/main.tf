07f5d9d9f8e5a0d4044dbfddcfe083447c3f861b
Rebecca Whitehurst-Martin <Rebecca.Whitehurst-Martin@digital.homeoffice.gov.uk>
2025-05-15 14:35:12 +0100

    CCL-2773 | simplify to default tags for test

--- aws_account_configuration/main.tf
+++ aws_account_configuration/main.tf
@@ -17,7 +17,8 @@ resource "dynatrace_aws_credentials" "aws_connection" {
     iam_role   = var.tenant_vars.iam_role
   }
   tagged_only                         = contains(keys(var.tenant_vars), "monitor_tags") ? true : false
-  tags_to_monitor                     = contains(keys(var.tenant_vars), "monitor_tags") ? var.tenant_vars.monitor_tags : null
+#  tags_to_monitor                     = contains(keys(var.tenant_vars), "monitor_tags") ? var.tenant_vars.monitor_tags : null
+  tags_to_monitor                     = {name = 'dynatrace', value = 'true'}
 }
 
 
