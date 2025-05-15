23f97410894f253a90473204ab92277416f7bf3d
Rebecca Whitehurst-Martin <Rebecca.Whitehurst-Martin@digital.homeoffice.gov.uk>
2025-05-15 11:58:03 +0100

    CCL-2773 | change boolean to list

--- aws_account_configuration/main.tf
+++ aws_account_configuration/main.tf
@@ -18,7 +18,7 @@ resource "dynatrace_aws_credentials" "aws_connection" {
   }
   tagged_only                         = contains(keys(var.tenant_vars), "monitor_tags") ? var.tenant_vars.monitor_tags : false
   dynamic "tags_to_monitor" {
-    for_each = contains(keys(var.tenant_vars), "monitor_tags") ? var.tenant_vars.monitor_tags : false
+    for_each = contains(keys(var.tenant_vars), "monitor_tags") ? var.tenant_vars.monitor_tags : []
     content {
       name  = each.key
       value = each.value
