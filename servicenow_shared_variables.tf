7c46d3eb3eb8e8bdc28946a42a8238caca4b72df
Becky <93141344+beckywhitemartin@users.noreply.github.com>
2025-06-13 15:56:45 +0100

    Ccl 3031 (#65)
    * CCL-3031 | sets vars to optional for envs where servicenow_integration is not used
    
    * CCL-3031 | sets vars to optional for envs where servicenow_integration is not used
    
    * CCL-3031 | sets vars to optional for envs where servicenow_integration is not used

--- servicenow_shared_variables.tf
+++ servicenow_shared_variables.tf
@@ -1,14 +1,16 @@
-# All the following variables must come from the 
-# AWS secret from inside the terragrunt pipeline 
+# All the following variables must come from the
+# AWS secret from inside the terragrunt pipeline
 # and would be passed as TF_VAR from the pipeline.
 variable "SERVICENOW_END_POINT" {
   description = "ServiceNow endpoint url."
   type        = string
+  default     = ""
 }
 
 variable "SERVICENOW_ENV_ID" {
   description = "The equivalent of the envid of the dynatrace. The id will be used as https://<this var>.service-now.com/oauth_token.do for oauth authentication."
   type        = string
+  default     = ""
 }
 
 variable "SERVICENOW_CLIENT_ID" {
@@ -16,6 +18,7 @@ variable "SERVICENOW_CLIENT_ID" {
   type        = string
   sensitive   = true
   #ephemeral = true
+  default    = ""
 }
 
 variable "SERVICENOW_CLIENT_SECRET" {
@@ -23,4 +26,5 @@ variable "SERVICENOW_CLIENT_SECRET" {
   type        = string
   sensitive   = true
   #ephemeral = true
+  default     = ""
 }
