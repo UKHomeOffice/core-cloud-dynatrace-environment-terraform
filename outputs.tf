f1a8ee191e4e3df5f4a37feebfecc15e0af768ae
Adam Hancock <adam.hancock@digital.homeoffice.gov.uk>
2025-06-23 09:27:47 +0100

    Add Dynatrace log storage module for labelled pods with necessary variables and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,6 @@
+# output for dynatrace_log_storage module
+output "dynatrace_log_storage_include_dynatrace_labelled_pods_id" {
+  description = "The ID of the Dynatrace log storage rule for Dynatrace labelled pods."
+  value       = module.dynatrace_log_storage_include_dynatrace_labelled_pods.dynatrace_log_storage_id
+}
 
