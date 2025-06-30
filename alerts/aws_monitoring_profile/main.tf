f97ea400c4dcf393651d4ad206d859fa5a4efdc0
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-06-30 13:09:16 +0100

    CCL-3255 updated aws_monitoring_profile_alert

--- alerts/aws_monitoring_profile/main.tf
+++ alerts/aws_monitoring_profile/main.tf
@@ -18,20 +18,13 @@ resource "dynatrace_slack_notification" "slack_alerts" {
 
 resource "dynatrace_alerting" "aws_monitoring_profile_alert" {
   name = "AWS Monitoring profile"
-  rules {
-     rule {
-        include_mode = var.aws_monitoring_profile_alerting_rules.include_mode
-        delay_in_minutes = var.aws_monitoring_profile_alerting_rules.delay_in_minutes
-        severity_level = "ERRORS" 
-        tags= var.aws_monitoring_profile_alerting_rules.tags
-      }
-    }
   filters {
     filter {
       custom{
         title {
         operator = "CONTAINS"
-        value = "AWS Integration"
+        value    = "AWS Monitoring"
+        enabled  = var.aws_monitoring_profile_alert_config.enabled
         }
       }
     }
--- dynatrace_servicenow_integration/variables.tf
+++ dynatrace_servicenow_integration/variables.tf
@@ -8,7 +8,12 @@ variable "servicenow_payload" {
 variable "servicenow_alerting_rules" {
   type = map(object({
     include_mode = optional(string, "NONE") # According to https://registry.terraform.io/providers/dynatrace-oss/dynatrace/latest/docs/resources/alerting#nested-schema-for-rulesrule, 'NONE' applies to all monitored entities
-    tags = optional(list(string), []) 
+      # TODO - This default tag value is to get the code working and 
+    # does not match the modules documentation in 
+    # https://registry.terraform.io/providers/dynatrace-oss/dynatrace/latest/docs/resources/alerting#nestedblock--rules - 
+    # potentially be a bug in the module - 
+    # Issue discussed in this conversation - https://hod-dsp.slack.com/archives/C0807UKF7RP/p1746542824632969
+    tags = optional(list(string), ["intentional_invalid_tag"])
     delay_in_minutes = optional(number, 0)
   }))
 }
