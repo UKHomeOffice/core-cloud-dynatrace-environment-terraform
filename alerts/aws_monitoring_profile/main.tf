30ac3d0b8d65338ce60470ef5cf51e2568cd9b07
mynampatinaveen <mynampatinaveen@users.noreply.github.com>
2025-07-02 17:03:20 +0100

    CCL-3255 changes aws monitoring profiles to use regex (#75)
    * CCL-3255 changes aws monitoring profiles to use regex
    
    * CCL-3255 changes aws monitoring profiles to use regex
    
    * CCL-3255 changes aws monitoring profiles to use regex
    
    * CCL-3255 changes aws monitoring profiles to use regex
    
    * CCL-3255 changes aws monitoring profiles to use regex
    
    ---------
    
    Co-authored-by: SrinivasanSundaram-HO <srinivasan.sundaram1@digital.homeoffice.gov.uk>

--- alerts/aws_monitoring_profile/main.tf
+++ alerts/aws_monitoring_profile/main.tf
@@ -18,12 +18,20 @@ resource "dynatrace_slack_notification" "slack_alerts" {
 
 resource "dynatrace_alerting" "aws_monitoring_profile_alert" {
   name = "AWS Monitoring profile"
+  rules {
+    rule {
+        include_mode = var.aws_monitoring_profile_alerting_rules.include_mode
+        delay_in_minutes = var.aws_monitoring_profile_alerting_rules.delay_in_minutes
+        severity_level = "ERRORS" 
+        tags= var.aws_monitoring_profile_alerting_rules.tags
+      }
+    }
   filters {
     filter {
       custom{
         title {
-        operator = "CONTAINS"
-        value    = "AWS Monitoring"
+        operator = "REGEX_MATCHES"
+        value    = "^AWS Monitoring profile (?>\\w+) is in ERROR State"
         enabled  = var.aws_monitoring_profile_alert_config.enabled
         }
       }
