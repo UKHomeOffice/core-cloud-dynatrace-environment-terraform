abf8d9b2a068efe2e97f0b4390af204e2eb89643
MOHAMMED-ho <Mohammed.Shareef@digital.homeoffice.gov.uk>
2025-07-03 13:49:22 +0100

    adding anomalies detection config (#77)

---
+++ anomaly_detection/k8s_node_anomalies.tf
@@ -0,0 +1,28 @@
+resource "dynatrace_k8s_node_anomalies" "core-cloud-k8s-node-anomalies" {
+  scope = "environment"
+  cpu_requests_saturation {
+    enabled = true
+    configuration {
+      observation_period_in_minutes = 6
+      sample_period_in_minutes      = 4
+      threshold                     = 95
+    }
+  }
+  memory_requests_saturation {
+    enabled = false
+  }
+  pods_saturation {
+    enabled = true
+    configuration {
+      observation_period_in_minutes = 6
+      sample_period_in_minutes      = 4
+      threshold                     = 95
+    }
+  }
+  readiness_issues {
+    enabled = false
+  }
+  node_problematic_condition {
+    enabled = false
+  }
+}
