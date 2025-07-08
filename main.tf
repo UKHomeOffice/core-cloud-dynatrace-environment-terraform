a25e54d6bae62744816169a498bfd524689f573a
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-07-08 11:48:47 +0100

    CCL-2240-c segregated metric from alerts

--- main.tf
+++ main.tf
@@ -24,16 +24,18 @@ module "dynatrace_management_zones" {
 
 module "ghes_alerts" {
   source            = "./alerts/ghes"
-  count             = contains(keys(var.tenant_vars), "ghes_alert") ? 1 : 0
+  count             = contains(keys(var.tenant_vars.ghees_alert),
+  "alerting_profile_name"
+  )? tomap(var.tenant_vars.ghes_alert.ghes_alert_configs) : tomap({})
   ghes_alert_configs = var.tenant_vars.ghes_alert_configs
+  }
 }
 
 module "metric_events" {
   source            = "./metric_events"
   count             = contains(keys(var.tenant_vars), "metric_events") ? 1 : 0
   common_metrics_vars = var.tenant_vars.metric_events.common_metrics_values
-  metrics_vars        =  var.tenant_vars.metric_events.metrics
-  
+  metrics_vars        =  var.tenant_vars.metric_events.metrics 
 }
 
 module "ghes_dashboards" {
