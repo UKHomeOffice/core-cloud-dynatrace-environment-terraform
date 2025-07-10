c929c36a2bc6eab3f2975e4684cb5a9b35dd10aa
Adam Hancock <adam.hancock@digital.homeoffice.gov.uk>
2025-07-10 15:52:16 +0100

    CCL-3278-fix-logging-rules-1

--- main.tf
+++ main.tf
@@ -125,26 +125,26 @@ module "anomaly_detection" {
   source = "./anomaly_detection/"
 }
 
-module "dynatrace_log_storage_include_dynatrace_labelled_pods" {
-  source          = "./dynatrace_log_storage"
-  name            = "Include Dynatrace Labelled Pods"
-  enabled         = true
-  send_to_storage = true
 
-// DT docs are wrong - Use https://github.com/dynatrace-oss/terraform-provider-dynatrace/blob/a02c5b5b61cfc1d216508202ec7e4f6bd4787069/dynatrace/api/builtin/logmonitoring/logstoragesettings/settings/enums.go#L47-L69 for valid values
-  matcher_attribute = "k8s.pod.label"
-  matcher_operator  = "MATCHES"
-  matcher_values    = ["dynatrace-logs=true"]
-}
+module "dynatrace_log_storage_rules" {
+  source = "./dynatrace_log_storage"
 
-
-module "dynatrace_log_storage_include_kube_system_namespace" {
-  source          = "./dynatrace_log_storage"
-  name            = "Include Kube-System Namespace"
-  enabled         = true
-  send_to_storage = true
-
-  matcher_attribute = "k8s.namespace.name"
-  matcher_operator  = "MATCHES"
-  matcher_values    = ["kube-system"]
+  rules = [
+  {
+        name              = "include-dynatrace-pods"
+        enabled           = true
+        send_to_storage    = true
+        matcher_attribute = "k8s.namespace.name"
+        matcher_operator  = "EQUALS"
+        matcher_values    = ["dynatrace"]
+  },
+  {
+      name              = "include-dt-containers"
+        enabled           = true
+        send_to_storage   = true
+        matcher_attribute = "k8s.pods.label"
+        matcher_operator = "MATCHES"
+        matcher_values = ["dynatrace-logs=true"]
+        }
+        ]
 }
