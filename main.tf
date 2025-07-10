624781b953e7c2854db5c78a638b3d3307553170
mynampatinaveen <mynampatinaveen@users.noreply.github.com>
2025-07-10 11:12:26 +0100

    CCL-2240-d add conditions check if keys exists (#87)
    * CCL-2240-d add conditions around variable delcaration
    
    * CCL-2240-d add conditions around variable delcaration
    
    * CCL-2240-d add conditions around variable delcaration
    
    ---------
    
    Co-authored-by: SrinivasanSundaram-HO <srinivasan.sundaram1@digital.homeoffice.gov.uk>

--- main.tf
+++ main.tf
@@ -25,13 +25,17 @@ module "dynatrace_management_zones" {
 module "ghes_alerts" {
   source             = "./alerts/ghes"
   count              = contains(keys(var.tenant_vars), "ghes_alert") ? 1 : 0
-  ghes_alert_configs = var.tenant_vars.ghes_alert.ghes_alert_configs
+  ghes_alert_configs = contains(keys(var.tenant_vars.ghes_alert), "ghes_alert_configs"
+    ) && var.tenant_vars.ghes_alert.ghes_alert_configs != null ? tomap(var.tenant_vars.ghes_alert.ghes_alert_configs
+  ) : tomap({})
 }
-
 
 module "metric_events" {
   source              = "./metric_events"
-  count               = contains(keys(var.tenant_vars), "metric_events") ? 1 : 0
+  count               = contains(keys(var.tenant_vars), "metric_events"
+    ) && contains(keys(var.tenant_vars.metric_events), "common_metric_values"
+    ) && contains(keys(var.tenant_vars.metric_events), "metrics"
+    ) && var.tenant_vars.metric_events.common_metric_values != null && var.tenant_vars.metric_events.metrics != null ? 1 : 0
   common_metrics_vars = var.tenant_vars.metric_events.common_metric_values
   metrics_vars        = var.tenant_vars.metric_events.metrics
 }
