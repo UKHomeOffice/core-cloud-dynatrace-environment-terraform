f061c58930c2385c8b6bd8bfdb49bff84536c659
Chris Freeman <christopher.freeman@digital.homeoffice.gov.uk>
2025-07-18 14:49:35 +0100

    CCL-3267 - BCP emails on GHES

--- alerts/ghes/main.tf
+++ alerts/ghes/main.tf
@@ -19,7 +19,6 @@ resource "dynatrace_slack_notification" "slack_alerts" {
 }
 
 
-
 resource "dynatrace_alerting" "ghes_alert_profiles" {
   for_each = var.ghes_alert_configs
 
@@ -72,11 +71,11 @@ resource "dynatrace_alerting" "ghes_alert_profiles" {
       }
     }
     filter {
-      custom{
+      custom {
         title {
-        operator = "CONTAINS"
-        value    = each.value.message_status
-        enabled  = each.value.filter_enabled
+          operator = "CONTAINS"
+          value    = each.value.message_status
+          enabled  = each.value.filter_enabled
         }
       }
     }
@@ -84,6 +83,62 @@ resource "dynatrace_alerting" "ghes_alert_profiles" {
   }
 }
 
+
+resource "dynatrace_email_notification" "bcp_email" {
+  count                  = var.bcp_alerting.enabled ? 1 : 0
+  active                 = true
+  name                   = var.bcp_alerting.email_name
+  profile                = dynatrace_alerting.bcp_alert_profile[0].id
+  subject                = var.bcp_alerting.email_subject
+  to                     = var.bcp_alerting.email_to
+  notify_closed_problems = false
+  body                   = <<EOT
+<b>Names of Impacted Entities:</b> "{NamesOfImpactedEntities}" <br/>
+<b>Priority:</b> P3 <br/>
+<b>Problem Title:</b> "{ProblemTitle}" <br/> 
+<b>Problem DetailsText:</b> "{ProblemDetailsText}" <br/>
+<b>Impacted Entities:</b> {ImpactedEntities} <br/>
+<b>Impacted Entity Names:</b> "{ImpactedEntityNames}" <br/>
+<b>Impacted Entity:</b> "{ImpactedEntity}" <br/>
+<b>PID:</b> "{PID}" <br/>
+<b>Problem ID:</b> "{ProblemID}" <br/>
+<b>Problem Impact:</b> "{ProblemImpact}" <br/> 
+<b>Problem Severity:</b> "{ProblemSeverity}" <br/>
+<b>Problem URL:</b> "{ProblemURL}" <br/> 
+<b>State:</b> "{State}" <br/>
+<b>Tags:</b> "{Tags}" <br/>
+
+{ProblemDetailsHTML}
+EOT
+}
+
+resource "dynatrace_alerting" "bcp_alert_profile" {
+  count = var.bcp_alerting.enabled ? 1 : 0
+  name  = var.bcp_alerting.alerting_profile_name
+
+  rules {
+    rule {
+      include_mode     = var.bcp_alerting.include_mode
+      delay_in_minutes = var.bcp_alerting.delay_in_minutes
+      severity_level   = "CUSTOM_ALERT"
+    }
+  }
+
+  filters {
+    filter {
+      custom {
+        metadata {
+          items {
+            filter {
+              key   = var.bcp_alerting.tag_key
+              value = var.bcp_alerting.tag_value
+            }
+          }
+        }
+      }
+    }
+  }
+}
 
 output "debug_ghes_alert_configs" {
   value = var.ghes_alert_configs
--- alerts/ghes/variables.tf
+++ alerts/ghes/variables.tf
@@ -1,19 +1,33 @@
 variable "ghes_alert_configs" {
   type = map(object({
     # generic config for ghes alerts
-    enabled                           = bool
-    alerting_profile_name             = string
-    notify_closed_problem             = bool
-    include_mode                      = string
-    delay_in_minutes                  = string
-    slack_notification_enabled        = bool
-    slack_message                     = string
-    slack_notification_name           = string
-    slack_url                         = string
-    channel_name                      = string
-    tag_key                           = string
-    tag_value                         = string
-    filter_enabled                    = string
-    message_status                    = string
+    enabled                    = bool
+    alerting_profile_name      = string
+    notify_closed_problem      = bool
+    include_mode               = string
+    delay_in_minutes           = string
+    slack_notification_enabled = bool
+    slack_message              = string
+    slack_notification_name    = string
+    slack_url                  = string
+    channel_name               = string
+    tag_key                    = string
+    tag_value                  = string
+    filter_enabled             = string
+    message_status             = string
   }))
+}
+
+variable "bcp_alerting" {
+  type = object({
+    enabled               = bool
+    alerting_profile_name = string
+    include_mode          = string
+    delay_in_minutes      = number
+    tag_key               = string
+    tag_value             = string
+    email_name            = string
+    email_subject         = string
+    email_to              = list(string)
+  })
 }
