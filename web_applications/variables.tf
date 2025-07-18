cd39bacbb4f8c3f57235f60821c3acac4f2f1a57
MOHAMMED-ho <mohammed.shareef@digital.homeoffice.gov.uk>
2025-07-18 14:09:33 +0100

    CCL-3535

--- web_applications/variables.tf
+++ web_applications/variables.tf
@@ -6,6 +6,10 @@ variable "web_application_name" {
   description = "The name of the web application, displayed in the UI"
   type = string
 }
+variable "web_application_id" {
+  description = "The id of the web application, displayed in the Url of the application"
+  type = string
+}
 variable "web_application_type" {
   description = "The type of the web application. Possible values are AUTO_INJECTED, BROWSER_EXTENSION_INJECTED and MANUALLY_INJECTED"
   type = string
