a876c965ba8fb301a70f574887939ed551317a0c
ishaikh-ho <iqbal.shaikh2@digital.homeoffice.gov.uk>
2025-07-30 14:30:15 +0100

    CCL-3561: Fixing typo (#109)

--- web_applications/variables.tf
+++ web_applications/variables.tf
@@ -1,11 +1,13 @@
 variable "web_application_name" {
   description = "The name of the web application, displayed in the UI"
-  type = string
+  type        = string
 }
 variable "web_application_type" {
   description = "The type of the web application. Possible values are AUTO_INJECTED, BROWSER_EXTENSION_INJECTED and MANUALLY_INJECTED"
-  type = string
+  type        = string
 }
 variable "rum_enabled" {
   description = "Real user monitoring enabled/disabled"
-  type = bool
+  type        = bool
+  default     = true
+}
