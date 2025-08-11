8a1fdbc9ed196b59dc396143ddde624d0d9238f9
SrinivasanSundaram-HO <srinivasan.sundaram1@digital.homeoffice.gov.uk>
2025-08-11 16:40:11 +0100

    Changed the aws secret structure to work for different urls for different channels (#128)

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "ghes_alerts" {
     email_subject         = ""
     email_to              = []
   }
-  SLACK_NOTIFICATION_URLS = var.GHES_SLACK_URLS
+  slack_webhook_urls = var.slack_webhook_urls
 }
 
 
@@ -133,8 +133,9 @@ module "dynatrace_aws_monitoring_profile_integration" {
   ) ? tomap(var.tenant_vars.aws_monitoring_profile.aws_monitoring_profile_rules) : tomap({})
 
   aws_monitoring_profile_alert_config = var.tenant_vars.aws_monitoring_profile_integration
-  SLACK_NOTIFICATION_URL = var.AWS_MONITORING_SLACK_URL
+  slack_webhook_url = var.slack_webhook_urls["aws_monitoring_profile"]
 }
+
 module "anomaly_detection" {
   source = "./anomaly_detection/"
 }
