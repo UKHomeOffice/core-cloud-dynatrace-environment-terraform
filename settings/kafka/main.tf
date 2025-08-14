dc79c3f2b03b484389851919f4e9dc6d377bec4d
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-08-14 16:19:23 +0100

    CC-124 add dynatrace_generic_setting module for kafka

--- settings/kafka/main.tf
+++ settings/kafka/main.tf
@@ -11,7 +11,8 @@ resource "dynatrace_generic_setting" "java_kafka_discover_listeners" {
   schema = "builtin:oneagent.features"
   scope  = "environment"
   value = jsonencode({
-    "enabled" : var.enabled
-    "name"    : "Java Kafka - discover listeners automatically"
+    "enabled"     : var.enabled
+    "name"        : "Java Kafka - discover listeners automatically"
+    "updateToken" : vu9U3hXY3q0ATAAkYTczZTgwYTYtYTIwYS0zOTVjLTg3NjgtMTg0NDIxYzU1ZjNjACQzYTgzZTc1MC03OTIxLTExZjAtODAwMS0wMTAwMDAwMDAwMDe-71TeFdjerQ
   })
 }
