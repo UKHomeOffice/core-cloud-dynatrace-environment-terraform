755c9b562179db285ad0a58e703935099274dd3e
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-09-03 16:39:57 +0100

    CCL-4629 mz refactor

--- dynatrace_management_zones/main.tf
+++ dynatrace_management_zones/main.tf
@@ -7,43 +7,70 @@ terraform {
   }
 }
 
-resource "dynatrace_management_zone_v2" "management_zone" {
-# Corresponds to the object structure defined in the variables.tf
-# One zone entity, consisting of 0 or 1 'rules' blocks - which in turn consist of 1 or more individual 'rule' definitions
+locals {
+  default_rules_raw = yamldecode(
+    templatefile("${path.module}/rules.tftpl", {
+      project_id           = try(var.zone_vars.project_id, "")
+      host_prefix          = try(var.zone_vars.host_prefix, "")
+      webapp_prefix        = try(var.zone_vars.webapp_prefix, "")
+      k8s_cluster_operator = try(var.zone_vars.k8s_cluster_operator, "EXISTS")
+      k8s_cluster_value    = try(var.zone_vars.k8s_cluster_value, "")
+      account_id           = try(var.zone_vars.account_id, "992382599151")
+    })
+  ).default_rules
 
-  name = var.zone_name
-  description = var.zone_vars.description
-  legacy_id = var.zone_vars.legacy_id
+  rules_map = {
+    for rule_key, rule_value in try(var.zone_vars.rules, {}) :
+    rule_key => merge(
+      try(local.default_rules_raw[rule_key], {}),
+      try(rule_value, {}),
+      {
+        enabled = try(local.default_rules_raw[rule_key].enabled, true)
+        type    = try(local.default_rules_raw[rule_key].type, "SELECTOR")
+      }
+    )
+  }
+
+  zone_vars_preprocessed = merge(
+    var.zone_vars,
+    { rules = local.rules_map }
+  )
+}
+
+resource "dynatrace_management_zone_v2" "management_zone" {
+  name        = var.zone_name
+  description = try(var.zone_vars.description, "Management zone for ${var.zone_name}")
+  legacy_id   = try(var.zone_vars.legacy_id, null)
+
   dynamic "rules" {
-    for_each = var.zone_vars.rules != null ? var.zone_vars.rules[*] : []
-    # Create a 'rules' block if defined in the config.yaml, else skips all following dynamic blocks
+    for_each = try(local.zone_vars_preprocessed.rules, null) != null ? [local.zone_vars_preprocessed.rules] : []
     content {
       dynamic "rule" {
-        for_each = var.zone_vars.rules
-        # Creates one rule definition per entry inside the 'rules' section of the MZ config (name not used)
+        for_each = rules.value
         content {
-            type = rule.value.type
-            enabled = rule.value.enabled
-            entity_selector = rule.value.entity_selector
+          type            = rule.value.type
+          enabled         = rule.value.enabled
+          entity_selector = try(rule.value.entity_selector, null)
 
           dynamic "attribute_rule" {
-            for_each = rule.value.attribute_rule[*]
-            # Creates an attribute rule block with conditions as defined - either this or dimension_rule
+            for_each = try(rule.value.attribute_rule, []) != [] ? [rule.value.attribute_rule] : []
             content {
-              azure_to_pgpropagation = attribute_rule.value.azure_to_pgpropagation
-              azure_to_service_propagation = attribute_rule.value.azure_to_service_propagation
-              custom_device_group_to_custom_device_propagation = attribute_rule.value.custom_device_group_to_custom_device_propagation
-              host_to_pgpropagation = attribute_rule.value.host_to_pgpropagation
-              pg_to_host_propagation = attribute_rule.value.pg_to_host_propagation
-              pg_to_service_propagation = attribute_rule.value.pg_to_service_propagation
-              service_to_host_propagation = attribute_rule.value.service_to_host_propagation
-              service_to_pgpropagation = attribute_rule.value.service_to_pgpropagation
-              entity_type = attribute_rule.value.entity_type
-              
-              attribute_conditions {
-                dynamic "condition" {
-                  for_each = try(attribute_rule.value.attribute_conditions, [])
-                  content {
+              azure_to_pgpropagation                  = try(attribute_rule.value.azure_to_pgpropagation, null)
+              azure_to_service_propagation            = try(attribute_rule.value.azure_to_service_propagation, null)
+              custom_device_group_to_custom_device_propagation = try(attribute_rule.value.custom_device_group_to_custom_device_propagation, null)
+              host_to_pgpropagation                   = try(attribute_rule.value.host_to_pgpropagation, null)
+              pg_to_host_propagation                  = try(attribute_rule.value.pg_to_host_propagation, null)
+              pg_to_service_propagation               = try(attribute_rule.value.pg_to_service_propagation, null)
+              service_to_host_propagation             = try(attribute_rule.value.service_to_host_propagation, null)
+              service_to_pgpropagation                = try(attribute_rule.value.service_to_pgpropagation, null)
+              entity_type                             = attribute_rule.value.entity_type
+
+              dynamic "attribute_conditions" {
+                for_each = try(attribute_rule.value.attribute_conditions, [])
+                content {
+                  dynamic "condition" {
+                    for_each = attribute_conditions.value
+                    content {
                       key                 = condition.value.key
                       operator            = condition.value.operator
                       case_sensitive      = try(condition.value.case_sensitive, null)
@@ -54,6 +81,7 @@ resource "dynatrace_management_zone_v2" "management_zone" {
                       integer_value       = try(condition.value.integer_value, null)
                       string_value        = try(condition.value.string_value, null)
                       tag                 = try(condition.value.tag, null)
+                    }
                   }
                 }
               }
@@ -61,28 +89,25 @@ resource "dynatrace_management_zone_v2" "management_zone" {
           }
 
           dynamic "dimension_rule" {
-            for_each = rule.value.dimension_rule[*]
-            # Creates a dimension rule block with conditions as defined - either this or attribute_rule
+            for_each = try(rule.value.dimension_rule, []) != [] ? [rule.value.dimension_rule] : []
             content {
               applies_to = dimension_rule.value.applies_to
               dimension_conditions {
                 condition {
                   condition_type = dimension_rule.value.dimension_conditions.condition.condition_type
-                  rule_matcher = dimension_rule.value.dimension_conditions.condition.rule_matcher
-                  value = dimension_rule.value.dimension_conditions.condition.value
-                  key = dimension_rule.value.dimension_conditions.condition.key
+                  rule_matcher   = dimension_rule.value.dimension_conditions.condition.rule_matcher
+                  value          = dimension_rule.value.dimension_conditions.condition.value
+                  key            = try(dimension_rule.value.dimension_conditions.condition.key, null)
                 }
               }
             }
           }
-
         }
       }
     }
   }
 }
 
-
-output "zone_var_output"{
-  value = "${var.zone_vars}"
+output "zone_var_output" {
+  value = local.zone_vars_preprocessed
 }
