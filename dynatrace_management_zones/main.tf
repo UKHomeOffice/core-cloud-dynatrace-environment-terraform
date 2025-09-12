166f4ac09b0c8756ddac5ef374d49f2144976888
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-09-12 14:17:12 +0100

    CCL-4629 mz refactor

--- dynatrace_management_zones/main.tf
+++ dynatrace_management_zones/main.tf
@@ -13,14 +13,17 @@ locals {
         project_id                           = var.zone_vars.project_id
         env_name                             = var.zone_vars.env_name
         tag_name                             = var.zone_vars.tag_name
-        tag_value                            = var.zone_vars.tag_value 
+        tag_value                            = var.zone_vars.tag_value
+        tag_env_name                         = var.zone_vars.env_name
         service_id                           = var.zone_vars.service_id
         webapp_prefix                        = var.zone_vars.webapp_prefix
-        k8s_cluster_value                    = var.zone_vars.k8s_cluster_value
+        # k8s_cluster_value                    = var.zone_vars.k8s_cluster_value
         k8s_cluster_name_begins_with         = var.zone_vars.k8s_cluster_name_begins_with
         k8s_cluster_name                     = var.zone_vars.k8s_cluster_name
+        k8s_namespace                        = var.zone_vars.k8s_namespace
         host_group_begins_with               = var.zone_vars.host_group_begins_with
         aws_account_id                       = var.zone_vars.aws_account_id
+        project_service                      = var.zone_vars.project_service
         pg_to_host_propagation               = var.zone_vars.pg_to_host_propagation
         pg_to_service_propagation            = var.zone_vars.pg_to_host_propagation
       })
@@ -30,7 +33,7 @@ locals {
     {
       for k, v in local.default_rules_raw :
       k => v
-      if contains(var.zone_vars.rules_templates, k)
+      if try(contains(var.zone_vars.rules_templates, k), false)
     },
     var.zone_vars.tenant_exclusive_rules
   )
