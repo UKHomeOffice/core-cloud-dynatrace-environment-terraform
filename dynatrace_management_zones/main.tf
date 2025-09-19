bd969650f0609a21dfa16e5807b3e0011927feda
mynampatinaveen <mynampatinaveen@users.noreply.github.com>
2025-09-19 14:29:49 +0100

    CCL-4629 Management zone refactor to reduce config in terragrunt (#138)
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * Modified for the rules templates and tenant-specific  rules
    
    * Changed the default value for the attribute_rule booleans to false (from null)
    
    * Fixed the dimension_condition bug
    
    * Removed the type in the template
    
    * Fixed the dimension_conditions
    
    * Fixed more bug in the module
    
    * Bug fix 2
    
    * Bug fix 3
    
    * Bug fix 4
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * Fixed bug in dimension_rules
    
    * (Hopefully) fixed another bug in the dimension_rule block
    
    * Another dimension_rule bug fix
    
    * Another debug
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    * CCL-4629 mz refactor
    
    ---------
    
    Co-authored-by: Srinivasan Sundaram <srinivasan.sundaram1@digital.homeoffice.gov.uk>

--- dynatrace_management_zones/main.tf
+++ dynatrace_management_zones/main.tf
@@ -8,37 +8,35 @@ terraform {
 }
 
 resource "dynatrace_management_zone_v2" "management_zone" {
-# Corresponds to the object structure defined in the variables.tf
-# One zone entity, consisting of 0 or 1 'rules' blocks - which in turn consist of 1 or more individual 'rule' definitions
+  name        = var.zone_name
+  description = try(var.zone_vars.description, "Management zone for ${var.zone_name}")
+  legacy_id   = try(var.zone_vars.legacy_id, null)
 
-  name = var.zone_name
-  description = var.zone_vars.description
-  legacy_id = var.zone_vars.legacy_id
   dynamic "rules" {
-    for_each = var.zone_vars.rules != null ? var.zone_vars.rules[*] : []
+    for_each = local.zone_rules_processed != null ? local.zone_rules_processed[*] : []
     # Create a 'rules' block if defined in the config.yaml, else skips all following dynamic blocks
     content {
       dynamic "rule" {
-        for_each = var.zone_vars.rules
+        for_each = local.zone_rules_processed
         # Creates one rule definition per entry inside the 'rules' section of the MZ config (name not used)
         content {
             type = rule.value.type
             enabled = rule.value.enabled
-            entity_selector = rule.value.entity_selector
+            entity_selector = try(rule.value.entity_selector, null)
 
           dynamic "attribute_rule" {
-            for_each = rule.value.attribute_rule[*]
+            for_each = try(rule.value.attribute_rule[*], {})
             # Creates an attribute rule block with conditions as defined - either this or dimension_rule
             content {
-              azure_to_pgpropagation = attribute_rule.value.azure_to_pgpropagation
-              azure_to_service_propagation = attribute_rule.value.azure_to_service_propagation
-              custom_device_group_to_custom_device_propagation = attribute_rule.value.custom_device_group_to_custom_device_propagation
-              host_to_pgpropagation = attribute_rule.value.host_to_pgpropagation
-              pg_to_host_propagation = attribute_rule.value.pg_to_host_propagation
-              pg_to_service_propagation = attribute_rule.value.pg_to_service_propagation
-              service_to_host_propagation = attribute_rule.value.service_to_host_propagation
-              service_to_pgpropagation = attribute_rule.value.service_to_pgpropagation
-              entity_type = attribute_rule.value.entity_type
+              azure_to_pgpropagation                           = try(attribute_rule.value.azure_to_pgpropagation, false)
+              azure_to_service_propagation                     = try(attribute_rule.value.azure_to_service_propagation, false)
+              custom_device_group_to_custom_device_propagation = try(attribute_rule.value.custom_device_group_to_custom_device_propagation, false)
+              host_to_pgpropagation                            = try(attribute_rule.value.host_to_pgpropagation, false)
+              pg_to_host_propagation                           = try(attribute_rule.value.pg_to_host_propagation, false)
+              pg_to_service_propagation                        = try(attribute_rule.value.pg_to_service_propagation, false)
+              service_to_host_propagation                      = try(attribute_rule.value.service_to_host_propagation, false)
+              service_to_pgpropagation                         = try(attribute_rule.value.service_to_pgpropagation, false)
+              entity_type                                      = try(attribute_rule.value.entity_type, null)
               
               attribute_conditions {
                 dynamic "condition" {
@@ -46,12 +44,12 @@ resource "dynatrace_management_zone_v2" "management_zone" {
                   content {
                       key                 = condition.value.key
                       operator            = condition.value.operator
-                      case_sensitive      = try(condition.value.case_sensitive, null)
+                      case_sensitive      = try(condition.value.case_sensitive, false)
                       dynamic_key         = try(condition.value.dynamic_key, null)
                       dynamic_key_source  = try(condition.value.dynamic_key_source, null)
                       entity_id           = try(condition.value.entity_id, null)
                       enum_value          = try(condition.value.enum_value, null)
-                      integer_value       = try(condition.value.integer_value, null)
+                      integer_value       = try(condition.value.integer_value, 0)
                       string_value        = try(condition.value.string_value, null)
                       tag                 = try(condition.value.tag, null)
                   }
@@ -61,28 +59,33 @@ resource "dynatrace_management_zone_v2" "management_zone" {
           }
 
           dynamic "dimension_rule" {
-            for_each = rule.value.dimension_rule[*]
+#            for_each = try(rule.value.dimension_rule[*],{})
+            for_each = ((try(rule.value.dimension_rule, null) != null) && (rule.value.dimension_rule != {})) ? rule.value.dimension_rule[*] : []
             # Creates a dimension rule block with conditions as defined - either this or attribute_rule
             content {
               applies_to = dimension_rule.value.applies_to
-              dimension_conditions {
-                condition {
-                  condition_type = dimension_rule.value.dimension_conditions.condition.condition_type
-                  rule_matcher = dimension_rule.value.dimension_conditions.condition.rule_matcher
-                  value = dimension_rule.value.dimension_conditions.condition.value
-                  key = dimension_rule.value.dimension_conditions.condition.key
+              dynamic "dimension_conditions" {
+                for_each = dimension_rule.value.dimension_conditions != null ? dimension_rule.value.dimension_conditions[*] : []
+                content {
+                  dynamic "condition" {
+                    for_each = try(dimension_rule.value.dimension_conditions[*],{})
+                    content {
+                      condition_type = try(condition.value.condition.condition_type, null)
+                      rule_matcher   = try(condition.value.condition.rule_matcher, null)
+                      value          = try(condition.value.condition.value, null)  
+                      key            = try(condition.value.key, null)
+                    }
+                  }
                 }
               }
             }
           }
-
         }
       }
     }
   }
 }
 
-
-output "zone_var_output"{
-  value = "${var.zone_vars}"
+output "zone_var_output" {
+  value = local.zone_rules_processed
 }
--- dynatrace_management_zones/variables.tf
+++ dynatrace_management_zones/variables.tf
@@ -1,56 +1,65 @@
 variable "zone_name" {
-# The name of the management zone - retrieved as the identifying key within the 'management_zones' block of the config.yaml
-  type = string
+  type        = string
+  description = "Name of the management zone"
 }
 
 variable "zone_vars" {
-#This variable consists of the content of the per-named Management Zone key from the config.yaml
-#The provided values are structured into an object, containing further nested objects, as below
   type = object({
-    description = optional(string)
-    legacy_id = optional(string)
-
-    rules = optional(map(object({
-    # The below attributes are contained in an individual 'rule' block created by the main TF file
-    # The 'rule' itself is dynamic and not defined as an object here, for cases where 'rules' are not defined
-    # ('Rules' are optional, but when set must contain at least one 'rule' block)
+    description                  = optional(string)
+    legacy_id                    = optional(string)
+    env_name                     = optional(string, "")
+    project_id                   = optional(string, "")
+    service_id                   = optional(string, "")
+    tag_env_name                 = optional(string, "")
+    webapp_prefix                = optional(string, "")
+    k8s_cluster_name_begins_with = optional(string, "")
+    k8s_cluster_name             = optional(string, "")
+    k8s_namespace                = optional(string, "")
+    host_group_begins_with       = optional(string, "")
+    aws_account_id               = optional(string,"")
+    project_service              = optional(string, "")
+    pg_to_host_propagation       = optional(bool, false)
+    pg_to_service_propagation    = optional(bool, false) 
+    rules_templates              = list(string)
+    tenant_exclusive_rules       = optional(map(object({
       enabled = bool
-      type = string
-      entity_selector = optional(string, "")
+      type    = string
+      entity_selector = optional(string)
       attribute_rule = optional(object({
-        azure_to_pgpropagation = optional(bool)
-        azure_to_service_propagation = optional(bool)
+        azure_to_pgpropagation                  = optional(bool)
+        azure_to_service_propagation            = optional(bool)
         custom_device_group_to_custom_device_propagation = optional(bool)
-        host_to_pgpropagation = optional(bool)
-        pg_to_host_propagation = optional(bool)
-        pg_to_service_propagation = optional(bool)
-        service_to_host_propagation = optional(bool)
-        service_to_pgpropagation = optional(bool)
+        host_to_pgpropagation                   = optional(bool)
+        pg_to_host_propagation                  = optional(bool)
+        pg_to_service_propagation               = optional(bool)
+        service_to_host_propagation             = optional(bool)
+        service_to_pgpropagation                = optional(bool)
         entity_type = string
         attribute_conditions = list(object({
-          key = string
-          operator = string
-          case_sensitive = optional(bool)
-          dynamic_key = optional(string)
-          dynamic_key_source = optional(string)
-          entity_id = optional(string)
-          enum_value = optional(string)
-          integer_value = optional(number)
-          string_value = optional(string)
-          tag = optional(string)
+          key                 = string
+          operator            = string
+          case_sensitive      = optional(bool)
+          dynamic_key         = optional(string)
+          dynamic_key_source  = optional(string)
+          entity_id           = optional(string)
+          enum_value          = optional(string)
+          integer_value       = optional(number)
+          string_value        = optional(string)
+          tag                 = optional(string)
         }))
       }))
       dimension_rule = optional(object({
-        applies_to = string
+        applies_to   = optional(string, "ANY")
         dimension_conditions = optional(object({
           condition = object({
             condition_type = string
-            rule_matcher = string
-            value = string
-            key = optional(string)
+            rule_matcher   = string
+            value          = string
+            key            = optional(string)
           })
         }))
       }))
     })))
   })
 }
+
