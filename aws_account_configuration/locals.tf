9e303a2e0819fa2629b852585fa308621df95767
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-10-09 16:36:54 +0100

    CC-438 exclude metric services

--- aws_account_configuration/locals.tf
+++ aws_account_configuration/locals.tf
@@ -1,14 +1,17 @@
 locals {
-  # Initialise the exclusive service map (to be empty if not defined)
-  exclusive_services = lookup(var.tenant_vars, "optional_exclusive_services", {})
+  # Ensure exclusive_services is a list
+  exclusive_services = lookup(var.tenant_vars, "optional_exclusive_services", [])
 
-  # Initialise the topup service map (to be empty if not defined)
+  # Filter defaults safely
+  filtered_defaults = {
+    for k, v in var.default_services :
+    k => v if !(contains(local.exclusive_services, k))
+  }
+
+  # Optional top-up services
   top_up_services = lookup(var.tenant_vars, "optional_services_top_up", {})
 
-  # Merge the topup service map with the default service map
-  computed_services = merge(var.default_services, local.top_up_services)
-
-  # Finally choose the exclusive service map *if it is not empty*
-  # over the computed service map
-  services_to_configure = length(local.exclusive_services) > 0 ? local.exclusive_services : local.computed_services
+  # Final services
+  services_to_configure = merge(local.filtered_defaults, local.top_up_services)
 }
+
