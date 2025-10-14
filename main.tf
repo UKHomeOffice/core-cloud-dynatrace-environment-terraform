ed34850e91f8cbd98788e8f75a735a5e8d4e219c
MOHAMMED-ho <mohammed.shareef@digital.homeoffice.gov.uk>
2025-10-14 22:26:38 +0100

    changes to the oam modules

--- main.tf
+++ main.tf
@@ -254,5 +254,5 @@ module "metric_stream" {
   env_name                        = each.value.env_name
   metrics_stream_name             = each.value.metrics_stream_name
   include_linked_accounts_metrics = each.value.include_linked_accounts_metrics
-  firehose_arn                    = module.firehose_dynatrace[var.metric_stream_to_firehose_map[each.key]].firehose_arn
+  firehose_arn                    = module.firehose_dynatrace[var.tenant_vars.metric_stream_to_firehose_map[each.key]].firehose_arn
 }
