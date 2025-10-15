f7086099e50ed6034f97bfb077c0645f33bef332
MOHAMMED-ho <mohammed.shareef@digital.homeoffice.gov.uk>
2025-10-15 11:13:20 +0100

    changes to the oam modules

--- firehose_dynatrace/main.tf
+++ firehose_dynatrace/main.tf
@@ -18,7 +18,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "backup_encryption
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3_backup_key.arn
     }
   }
 }
