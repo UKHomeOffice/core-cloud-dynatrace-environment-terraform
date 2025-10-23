5e711853a586f1e135fc02bf65f0635126d94f00
Iqbal Shaikh <iqbal.shaikh2@digital.homeoffice.gov.uk>
2025-10-23 21:09:58 +0100

    CCL-5810: Adding IAM policies

---
+++ aws_cw_logs/iam.tf
@@ -0,0 +1,70 @@
+resource "aws_iam_role" "cc_cosmos_cwl_firehose_access_role" {
+  name = "cc-cosmos-cwl-firehose-access-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "firehose.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "cc_cosmos_cwl_firehose_s3_kms_policy" {
+  name        = "cc-cosmos-cwl-firehose-s3-kms-policy"
+  description = "Policy for Firehose roles to access S3 bucket and KMS key"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "S3Access"
+        Effect = "Allow"
+        Action = [
+          "s3:AbortMultipartUpload",
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:ListBucketMultipartUploads",
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ]
+        Resource = [
+          aws_s3_bucket.cwl_backup_bucket.arn,
+          "${aws_s3_bucket.cwl_backup_bucket.arn}/*"
+        ]
+      },
+      {
+        Sid    = "CloudWatchLogsAccess"
+        Effect = "Allow"
+        Action = [
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "KMSAccess"
+        Effect = "Allow"
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = aws_kms_key.cc_cosmos_s3_kms_key.arn
+
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "firehose_s3_policy_attachment" {
+  role       = aws_iam_role.cc_cosmos_cwl_firehose_access_role.name
+  policy_arn = aws_iam_policy.cc_cosmos_cwl_firehose_s3_kms_policy.arn
+}
---
+++ aws_cw_logs/kms.tf
@@ -0,0 +1,63 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "cc_cosmos_s3_kms_key" {
+  description         = "KMS CMK for S3 encryption"
+  enable_key_rotation = true
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowRootAccountFullAccess",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action   = "kms:*",
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowFirehoseServiceToUseKey",
+        Effect = "Allow",
+        Principal = {
+          Service = "firehose.amazonaws.com"
+        },
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        Resource = "*",
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+        }
+      },
+      {
+        Sid    = "AllowFirehoseRolesToUseKey",
+        Effect = "Allow",
+        Principal = {
+          AWS = [
+            aws_iam_role.cc_cosmos_cwl_firehose_access_role.arn
+          ]
+        },
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_kms_alias" "cc_cosmos_firehose_s3_kms_alias" {
+  name          = "alias/cc-cosmos-firehose-s3-key"
+  target_key_id = aws_kms_key.cc_cosmos_s3_kms_key.id
+}
