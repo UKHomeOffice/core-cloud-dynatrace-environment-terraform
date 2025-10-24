b64063e2038e6485bfa3ae44f48d44c2e4de2f2f
ishaikh-ho <iqbal.shaikh2@digital.homeoffice.gov.uk>
2025-10-24 11:49:01 +0100

    CCL-5810: Adding IAM policies (#169)
    * CCL-5810: Adding IAM policies
    
    * CCL-5810: Adding tags
    
    * CCL-5810: Adding tags

--- aws_cw_logs/outputs.tf
+++ aws_cw_logs/outputs.tf
@@ -2,3 +2,11 @@ output "cwl_backup_bucket" {
   description = "The name of the S3 bucket created for CloudWatch Logs backup."
   value       = aws_s3_bucket.cwl_backup_bucket.bucket
 }
+
+output "firehose_access_role_arn" {
+  value = aws_iam_role.cc_cosmos_cwl_firehose_access_role.arn
+}
+output "kms_key_arn" {
+  description = "The ARN of the KMS key used for S3 encryption."
+  value       = aws_kms_alias.cc_cosmos_firehose_s3_kms_alias.arn
+}
