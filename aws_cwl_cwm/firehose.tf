de4bfd9c8800ae2e4b9bb85f0012da516d3fde07
MOHAMMED-ho <mohammed.shareef@digital.homeoffice.gov.uk>
2025-11-01 13:23:33 +0000

    fixing firehose mosule

--- aws_cwl_cwm/firehose.tf
+++ aws_cwl_cwm/firehose.tf
@@ -27,14 +27,18 @@ resource "aws_kinesis_firehose_delivery_stream" "dynatrace_http_stream" {
 
       # only for CloudWatch metrics
       dynamic "common_attributes" {
-       for_each = var.ingestion_type == "metrics" ? [1] : []
-       content {
-          name  = local.dynatrace_url_param_name
-          value = data.aws_secretsmanager_secret_version.dt_endpoint_internal.secret_string
-        }
-      }
+       for_each = var.ingestion_type == "metrics" ? [
+    {
+      name  = local.dynatrace_url_param_name
+      value = data.aws_secretsmanager_secret_version.dt_endpoint_internal.secret_string
     }
-
+  ] : []
+  content {
+    name  = common_attributes.value.name
+    value = common_attributes.value.value
+        }
+      } 
+  }
     # for failed puts to destination 
     s3_configuration {
       role_arn            = aws_iam_role.cc_cosmos_cwl_firehose_access_role.arn
