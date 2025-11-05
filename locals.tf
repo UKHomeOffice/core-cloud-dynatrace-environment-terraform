cbf42a7d021c8420380efb7da70ecd25780a8806
Tom Carpenter <thomas.carpenter@digital.homeoffice.gov.uk>
2025-11-05 13:51:08 +0000

    CCL-5832: fix local to reference try() on key in tenant vars

--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
-  files = var.tenant_vars.platform_dashboards ? fileset("${path.module}/dashboards/platform_dashboards/files", "*.json") : []
+  platform_dashboards_enabled = contains(keys(var.tenant_vars), "platform_dashboards") ? true : false
+  files = local.platform_dashboards_enabled ? fileset("${path.module}/dashboards/platform_dashboards/files", "*.json") : []
 }
