d688d32e30d3476dc789437da4d3ee47cf171285
Tom Carpenter <thomas.carpenter@digital.homeoffice.gov.uk>
2025-11-19 10:29:37 +0000

    CCL-6033: adding tag based scoping for web_applications

--- dynatrace_management_zones/locals.tf
+++ dynatrace_management_zones/locals.tf
@@ -1,20 +1,20 @@
 locals {
   default_rules_raw = yamldecode(
-      templatefile("${path.module}/rules.tftpl", {
-        project_id                           = var.zone_vars.project_id
-        env_name                             = var.zone_vars.env_name
-        tag_env_name                         = var.zone_vars.env_name
-        service_id                           = var.zone_vars.service_id
-        webapp_prefix                        = var.zone_vars.webapp_prefix
-        k8s_cluster_name_begins_with         = var.zone_vars.k8s_cluster_name_begins_with
-        k8s_cluster_name                     = var.zone_vars.k8s_cluster_name
-        k8s_namespace                        = var.zone_vars.k8s_namespace
-        host_group_begins_with               = var.zone_vars.host_group_begins_with
-        aws_account_id                       = var.zone_vars.aws_account_id
-        project_service                      = var.zone_vars.project_service
-        pg_to_host_propagation               = var.zone_vars.pg_to_host_propagation
-        pg_to_service_propagation            = var.zone_vars.pg_to_host_propagation
-      })
+    templatefile("${path.module}/rules.tftpl", {
+      project_id                   = var.zone_vars.project_id
+      env_name                     = var.zone_vars.env_name
+      tag_env_name                 = var.zone_vars.env_name
+      service_id                   = var.zone_vars.service_id
+      webapp_prefix                = var.zone_vars.webapp_prefix
+      k8s_cluster_name_begins_with = var.zone_vars.k8s_cluster_name_begins_with
+      k8s_cluster_name             = var.zone_vars.k8s_cluster_name
+      k8s_namespace                = var.zone_vars.k8s_namespace
+      host_group_begins_with       = var.zone_vars.host_group_begins_with
+      aws_account_id               = var.zone_vars.aws_account_id
+      project_service              = var.zone_vars.project_service
+      pg_to_host_propagation       = var.zone_vars.pg_to_host_propagation
+      pg_to_service_propagation    = var.zone_vars.pg_to_host_propagation
+    })
   ).default_rules
 
   zone_rules_processed = merge(
--- dynatrace_management_zones/main.tf
+++ dynatrace_management_zones/main.tf
@@ -20,9 +20,9 @@ resource "dynatrace_management_zone_v2" "management_zone" {
         for_each = local.zone_rules_processed
         # Creates one rule definition per entry inside the 'rules' section of the MZ config (name not used)
         content {
-            type = rule.value.type
-            enabled = rule.value.enabled
-            entity_selector = try(rule.value.entity_selector, null)
+          type            = rule.value.type
+          enabled         = rule.value.enabled
+          entity_selector = try(rule.value.entity_selector, null)
 
           dynamic "attribute_rule" {
             for_each = try(rule.value.attribute_rule[*], {})
@@ -37,21 +37,21 @@ resource "dynatrace_management_zone_v2" "management_zone" {
               service_to_host_propagation                      = try(attribute_rule.value.service_to_host_propagation, false)
               service_to_pgpropagation                         = try(attribute_rule.value.service_to_pgpropagation, false)
               entity_type                                      = try(attribute_rule.value.entity_type, null)
-              
+
               attribute_conditions {
                 dynamic "condition" {
                   for_each = try(attribute_rule.value.attribute_conditions, [])
                   content {
-                      key                 = condition.value.key
-                      operator            = condition.value.operator
-                      case_sensitive      = try(condition.value.case_sensitive, false)
-                      dynamic_key         = try(condition.value.dynamic_key, null)
-                      dynamic_key_source  = try(condition.value.dynamic_key_source, null)
-                      entity_id           = try(condition.value.entity_id, null)
-                      enum_value          = try(condition.value.enum_value, null)
-                      integer_value       = try(condition.value.integer_value, 0)
-                      string_value        = try(condition.value.string_value, null)
-                      tag                 = try(condition.value.tag, null)
+                    key                = condition.value.key
+                    operator           = condition.value.operator
+                    case_sensitive     = try(condition.value.case_sensitive, false)
+                    dynamic_key        = try(condition.value.dynamic_key, null)
+                    dynamic_key_source = try(condition.value.dynamic_key_source, null)
+                    entity_id          = try(condition.value.entity_id, null)
+                    enum_value         = try(condition.value.enum_value, null)
+                    integer_value      = try(condition.value.integer_value, 0)
+                    string_value       = try(condition.value.string_value, null)
+                    tag                = try(condition.value.tag, null)
                   }
                 }
               }
@@ -59,7 +59,7 @@ resource "dynatrace_management_zone_v2" "management_zone" {
           }
 
           dynamic "dimension_rule" {
-#            for_each = try(rule.value.dimension_rule[*],{})
+            #            for_each = try(rule.value.dimension_rule[*],{})
             for_each = ((try(rule.value.dimension_rule, null) != null) && (rule.value.dimension_rule != {})) ? rule.value.dimension_rule[*] : []
             # Creates a dimension rule block with conditions as defined - either this or attribute_rule
             content {
@@ -68,11 +68,11 @@ resource "dynatrace_management_zone_v2" "management_zone" {
                 for_each = dimension_rule.value.dimension_conditions != null ? dimension_rule.value.dimension_conditions[*] : []
                 content {
                   dynamic "condition" {
-                    for_each = try(dimension_rule.value.dimension_conditions[*],{})
+                    for_each = try(dimension_rule.value.dimension_conditions[*], {})
                     content {
                       condition_type = try(condition.value.condition.condition_type, null)
                       rule_matcher   = try(condition.value.condition.rule_matcher, null)
-                      value          = try(condition.value.condition.value, null)  
+                      value          = try(condition.value.condition.value, null)
                       key            = try(condition.value.key, null)
                     }
                   }
