1316cf9c0ff505062d4d689bac4273ef25266704
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2025-12-10 12:49:21 +0000

    CCL-6321 Enable Prometheus Metric Monitoring in Dynatrace Globally

---
+++ prometheus/main.tf
@@ -0,0 +1,23 @@
+resource "dynatrace_k8s_monitoring" "prometheus_monitoring" {
+  cloud_application_pipeline_enabled = var.cloud_application_pipeline_enabled
+  event_processing_active            = var.event_processing_active
+  filter_events                      = var.filter_events
+  include_all_fdi_events             = var.include_all_fdi_events
+  open_metrics_builtin_enabled       = var.open_metrics_builtin_enabled
+  open_metrics_pipeline_enabled      = var.open_metrics_pipeline_enabled
+
+  # Only create event_patterns if at least 1 pattern exists
+  dynamic "event_patterns" {
+    for_each = length(var.event_patterns) > 0 ? [1] : []
+    content {
+      dynamic "event_pattern" {
+        for_each = var.event_patterns
+        content {
+          active  = event_pattern.value.active
+          label   = event_pattern.value.label
+          pattern = event_pattern.value.pattern
+        }
+      }
+    }
+  }
+}
