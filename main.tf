48e4ad822e98f8bf173aa4e6503ce4e6840114ca
Iqbal Shaikh <iqbal.shaikh2@digital.homeoffice.gov.uk>
2026-01-09 13:43:06 +0000

    CCL-6061: Refactoring CWL/CWM structure

--- main.tf
+++ main.tf
@@ -262,9 +262,10 @@ module "aws_cwl_s3_bucket" {
   s3_backup_bucket_name     = each.value.s3_backup_bucket_name
   lifecycle_expiration_days = each.value.lifecycle_expiration_days
   #firehose config
-  ingestion_type            = each.value.ingestion_type
-  firehose_access_role_name = each.value.firehose_access_role_name
-  aws_kms_alias_firehose    = each.value.aws_kms_alias_firehose
+  ingestion_type                             = each.value.ingestion_type
+  firehose_access_role_name                  = each.value.firehose_access_role_name
+  aws_kms_alias_firehose                     = each.value.aws_kms_alias_firehose
+  cc_cosmos_firehose_s3_logs_kms_policy_name = each.value.cc_cosmos_firehose_s3_logs_kms_policy_name
   #dt config
   dt_logs_api_endpoint_name = each.value.dt_logs_api_endpoint_name
   dt_cwl_api_token_name     = each.value.dt_cwl_api_token_name
