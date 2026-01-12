aa7b73155e534ad75916b779c8a49221fe5e93bf
ishaikh-ho <iqbal.shaikh2@digital.homeoffice.gov.uk>
2026-01-12 09:57:36 +0000

    CCL-6061: Refactoring CWL/CWM structure (#210)
    * CCL-6061: Refactoring CWL/CWM structure
    
    * CCL-6061: Refactoring CWL/CWM structure
    
    * CCL-6061: Refactoring CWL/CWM structure
    
    * CCL-6061: Refactoring CWL/CWM structure
    
    * CCL-6061: Refactoring CWL/CWM structure

--- aws_cwl_cwm/firehose.tf
+++ aws_cwl_cwm/firehose.tf
@@ -4,12 +4,18 @@ locals {
   dynatrace_ingest_url = var.ingestion_type == "metrics" ? data.aws_secretsmanager_secret_version.dt_endpoint.secret_string : data.aws_secretsmanager_secret_version.dt_logs_api_endpoint.secret_string
 
   dynatrace_access_key = var.ingestion_type == "metrics" ? data.aws_secretsmanager_secret_version.cw_metrics_api_token.secret_string : data.aws_secretsmanager_secret_version.cw_logs_api_token.secret_string
+
+  firehose_name      = var.ingestion_type == "metrics" ? "cc-cosmos-cwm-firehose" : "cc-cosmos-cwl-firehose"
+  cw_log_group_name  = "${local.firehose_name}-log-group"
+  cw_log_stream_name = "${local.firehose_name}-log-stream"
+  s3_backup_prefix   = "backup/failed/"
+  s3_error_prefix    = "errors/"
 }
 
 # This Firehose stream delivers CloudWatch metrics or logs to a Dynatrace HTTP endpoint.
 # It supports backup to S3 for failed records and includes buffering, retry, and logging configurations.
 resource "aws_kinesis_firehose_delivery_stream" "dynatrace_http_stream" {
-  name        = var.firehose_name
+  name        = local.firehose_name
   destination = "http_endpoint"
 
   http_endpoint_configuration {
@@ -39,8 +45,8 @@ resource "aws_kinesis_firehose_delivery_stream" "dynatrace_http_stream" {
     s3_configuration {
       role_arn            = aws_iam_role.cc_cosmos_cwl_firehose_access_role.arn
       bucket_arn          = aws_s3_bucket.cwl_backup_bucket.arn
-      prefix              = var.s3_backup_prefix
-      error_output_prefix = var.s3_error_prefix
+      prefix              = local.s3_backup_prefix
+      error_output_prefix = local.s3_error_prefix
 
       buffering_size     = var.buffering_size
       buffering_interval = var.buffering_interval
@@ -52,8 +58,8 @@ resource "aws_kinesis_firehose_delivery_stream" "dynatrace_http_stream" {
 
     cloudwatch_logging_options {
       enabled         = true
-      log_group_name  = var.cw_log_group_name
-      log_stream_name = var.cw_log_stream_name
+      log_group_name  = local.cw_log_group_name
+      log_stream_name = local.cw_log_stream_name
     }
   }
   server_side_encryption {
--- aws_cwl_cwm/variables.tf
+++ aws_cwl_cwm/variables.tf
@@ -4,18 +4,6 @@ variable "s3_backup_bucket_name" {
   description = "Name of the S3 bucket to be created for failed CloudWatch Logs"
 }
 
-variable "s3_encryption_algorithm" {
-  type        = string
-  default     = "AES256"
-  description = "S3 bucket encryption algorithm"
-}
-
-variable "versioning_status" {
-  description = "S3 bucket versioning status"
-  type        = string
-  default     = "Suspended"
-}
-
 variable "tags" {
   description = "A map of tags to assign to the resource."
   type        = map(string)
@@ -25,11 +13,6 @@ variable "lifecycle_expiration_days" {
   description = "Number of days to keep objects before expiration"
   type        = number
   default     = 10
-}
-
-variable "firehose_name" {
-  type        = string
-  description = "Name of the Firehose delivery stream"
 }
 
 variable "dt_cwl_api_token_name" {
@@ -52,28 +35,6 @@ variable "dt_endpoint_internal_name" {
 
 variable "dt_logs_api_endpoint_name" {
   description = "The destination endpoint for the Dynatrace logs ingestion"
-  type        = string
-}
-
-variable "s3_backup_prefix" {
-  type        = string
-  default     = "backup/failed/"
-  description = "Prefix in S3 for backup of failed records"
-}
-
-variable "s3_error_prefix" {
-  type        = string
-  default     = "errors/"
-  description = "Prefix in S3 for error output"
-}
-
-variable "cw_log_group_name" {
-  description = "The name of the CloudWatch log group for Firehose logging"
-  type        = string
-}
-
-variable "cw_log_stream_name" {
-  description = "The name of the CloudWatch log stream for Firehose logging"
   type        = string
 }
 
@@ -119,7 +80,7 @@ variable "aws_kms_alias_firehose" {
 
 }
 variable "cc_cosmos_firehose_s3_logs_kms_policy_name" {
-
+  default     = "cc-cosmos-firehose-s3-logs-kms-policy"
   description = "name of the kms policy"
   type        = string
 }
