bb0528c90a5f327e0722fb3d33a03f67a96d62a2
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2026-01-27 15:44:34 +0000

    CCL-6775 update hub extensions to work for SSL and hadoop

--- hub_extensions/outputs.tf
+++ hub_extensions/outputs.tf
@@ -2,7 +2,3 @@ output "hub_extension_config_id" {
   description = "The ID of the Dynatrace Hub Extension Config resource."
   value       = dynatrace_hub_extension_config.hub_extension.id
 }
-
-
-
-
